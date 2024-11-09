e095d9f90d4262c3f6bbfa03308c481de4b899ef
alexe <oyleksa@gmail.com>
2024-11-09 21:57:19 +0100

    The third try

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int x = 5;
         System.out.println("Hello world!" +
                 "\nWe need to make a change." +
                 "\nJak poszło?");
+        System.out.println(x * 3);
     }
 }
